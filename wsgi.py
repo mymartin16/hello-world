09c659b802de18ea9ed64051672314bdc58abf99
Shaswat Kumar <39943930+mymartin16@users.noreply.github.com>
2018-06-05 17:36:45 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def main():
-    return ("Vikas")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     application.run()
